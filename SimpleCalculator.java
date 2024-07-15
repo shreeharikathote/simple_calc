e495ca3c0193b6ffc73460a8d10cc78c81fda240
01fe22bca086 <01fe22bca086@kletech.ac.in>
2024-07-15 13:25:34 +0530

    first commit

---
+++ SimpleCalculator.java
@@ -0,0 +1,29 @@
+public class SimpleCalculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        SimpleCalculator calc = new SimpleCalculator();
+        System.out.println("Addition: " + calc.add(5, 3));
+        System.out.println("Subtraction: " + calc.subtract(5, 3));
+        System.out.println("Multiplication: " + calc.multiply(5, 3));
+        System.out.println("Division: " + calc.divide(5, 3));
+    }
+}
